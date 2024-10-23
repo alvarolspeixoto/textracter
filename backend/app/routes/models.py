8077dcb2f9ed8ca4ba949100c142218ade8ae1cd
Álvaro Peixoto <alvaro.peixoto@dcomp.ufs.br>
2024-10-22 22:56:01 -0300

    refactor: cria models p/ melhorar retorno da api

---
+++ backend/app/routes/models.py
@@ -0,0 +1,56 @@
+from enum import Enum
+from pydantic import BaseModel
+
+class Type(Enum):
+    QUERY = "QUERY"
+    QUERY_RESULT = "QUERY_RESULT"
+    WORD = "WORD"
+
+class BoundingBox(BaseModel):
+    Width: float
+    Height: float
+    Left: float
+    Top: float
+
+class Point(BaseModel):
+    X: float
+    Y: float
+
+class Geometry(BaseModel):
+    BoundingBox: BoundingBox
+    Polygon: list[Point]
+
+class BaseBlock(BaseModel):
+    Confidence: float
+    Text: str
+    TextType: str
+    Geometry: Geometry
+    Id: str
+
+class DocumentMetadata(BaseModel):
+    Pages: int
+
+class WordBlock(BaseBlock):
+    BlockType: Type = Type.WORD
+
+class QueryBlock(BaseModel):
+    BlockType: Type = Type.QUERY
+    Id: str
+    Relationships: list[dict]
+    Query: dict
+
+class Relationship(BaseModel):
+    Type: str
+    Ids: list[str]
+
+class Query(BaseModel):
+    Text: str
+
+class QueryResultBlock(BaseBlock):
+    BlockType: Type = Type.QUERY_RESULT
+
+class TextractResponse(BaseModel):
+    DocumentMetadata: DocumentMetadata
+    Blocks: list[WordBlock | QueryBlock | QueryResultBlock]
+    AnalyzeDocumentModelVersion: str
+    ResponseMetadata: dict
