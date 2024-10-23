f78ebdeaa6e0180715c1a3d9b94281892f9dd4dd
Álvaro Peixoto <alvaro.peixoto@dcomp.ufs.br>
2024-10-23 20:20:23 -0300

    chore: altera o cors para aceitar todas as origens

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.routes import analyze_document
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(analyze_document.router)
 
 @app.get("/api")
